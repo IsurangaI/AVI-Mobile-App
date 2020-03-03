00d226856842873cd847e3d2852ac9c36f752e75
Isuranga Iniyage <47548926+nigma97@users.noreply.github.com>
2020-03-03 17:38:21 +0530

    init 3

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -7,23 +7,36 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new _MyAppState();
-    
-  }
-  
+}
+
 class _MyAppState extends State<MyApp> {
   File _image;
-  Future getImage() async{
+  Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
     setState(() {
-      _image= image;
-      
+      _image = image;
     });
-
-
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return new MaterialApp(
+      title: 'Image Picker',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Image Picker'),
+        ),
+        body: new Center(
+          child: _image == null
+              ? new Text('No image selected')
+              : new Image.file(_image),
+        ),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: getImage,
+          tooltip: 'Pick Image',
+          child: new Icon(Icons.camera),
+        ),
+      ),
+    );
   }
+}
