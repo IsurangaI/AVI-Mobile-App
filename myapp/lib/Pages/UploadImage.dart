f2196be5a8bdb6d86a85d9316b7888e9526c74b3
Shehana Iqbal <iqbalshehana@gmail.com>
2020-03-16 11:33:33 +0530

    drawer menu change

--- myapp/lib/Pages/UploadImage.dart
+++ myapp/lib/Pages/UploadImage.dart
@@ -51,21 +51,16 @@ class _MyAppState extends State<UploadImage> {
           child: ListView(
             padding: EdgeInsets.zero,
             children: const <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Text(
-                  'Drawer Header',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
+              UserAccountsDrawerHeader(
+                  accountName: Text('Shehana Iqbal'),
+                  accountEmail: Text('iqbalshehana@gmail.com')),
               ListTile(
                 leading: Icon(Icons.message),
                 title: Text('Messages'),
+              ),
+              ListTile(
+                leading: Icon(Icons.history),
+                title: Text('Recent History'),
               ),
               ListTile(
                 leading: Icon(Icons.account_circle),
