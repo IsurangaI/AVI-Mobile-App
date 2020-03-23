45d9c84f0ff6cbd141c3cc069aaa319877cb4e6e
Isuranga Iniyage <47548926+nigma97@users.noreply.github.com>
2020-03-23 10:14:47 +0530

    abc

---
+++ myapp/lib/Pages/login.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import './camera.dart';
+
+void main() => runApp(new Login());
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder>{
+        '/signup': (BuildContext context) => new Camera()
+      },
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        backgroundColor: Colors.amber,
+        resizeToAvoidBottomPadding: false,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                    child: Text('Vehicle Numberplate',
+                        style: TextStyle(
+                            fontSize: 35.0, fontWeight: FontWeight.bold)),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(30.0, 155.0, 0.0, 0.0),
+                    child: Text('Detection System',
+                        style: TextStyle(
+                            fontSize: 35.0, fontWeight: FontWeight.bold)),
+                  ),
+                  
+                ],
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'EMAIL',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.green))),
+                    ),
+                    SizedBox(height: 20.0),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'PASSWORD',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black))),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 5.0),
+                    Container(
+                      alignment: Alignment(1.0, 0.0),
+                      padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                    ),
+                    SizedBox(height: 40.0),
+                    Container(
+                      height: 40.0,
+                      child: Material(
+                        borderRadius: BorderRadius.circular(20.0),
+                        shadowColor: Colors.black,
+                        color: Colors.black,
+                        elevation: 7.0,
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Center(
+                            child: Text(
+                              'LOGIN',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    // Container(
+                    //   height: 40.0,
+                    //   color: Colors.transparent,
+                    //   child: Container(
+                    //     decoration: BoxDecoration(
+                    //         border: Border.all(
+                    //             color: Colors.black,
+                    //             style: BorderStyle.solid,
+                    //             width: 1.0),
+                    //         color: Colors.transparent,
+                    //         borderRadius: BorderRadius.circular(20.0)),
+                    //   ),
+                    // )
+                  ],
+                )),
+            SizedBox(height: 15.0),
+          ],
+        ));
+  }
+}
--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import './Pages/Loading.dart';
-import './Pages/UploadImage.dart';
-import 'Pages/HomePage.dart';
-import './Pages/History.dart';
+// import './Pages/Loading.dart';
+// import './Pages/UploadImage.dart';
+// import 'Pages/HomePage.dart';
+// import './Pages/History.dart';
+// import './Pages/camera.dart';
+import 'pages/login.dart';
+
 
 void main(){
   runApp(new MaterialApp(
-    home: new History(),
+    home: new Login(),
   ));
 }
