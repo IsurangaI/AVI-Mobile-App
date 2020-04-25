9bb28d9f48a545c239847a948f4e22d312d84111
Isuranga Iniyage <47548926+nigma97@users.noreply.github.com>
2020-04-25 11:24:42 +0530

    1

--- myapp/lib/Pages/Home.dart
+++ myapp/lib/Pages/Home.dart
@@ -187,10 +187,10 @@ class _MyHomePageState extends State<MyHomePage> {
             ]),
       ),
 // This trailing comma makes auto-formatting nicer for build methods.
-      floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
-        child: Icon(Icons.camera),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: getImage,
+      //   child: Icon(Icons.camera),
+      // ),
     );
   }
 }
